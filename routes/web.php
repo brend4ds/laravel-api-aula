6ae93cf027c7c03ec30898e9a75113f8adaafe7f
brend4ds <brendaamaral014@gmail.com>
2024-10-15 09:20:12 -0300

    aula 15/10

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,16 @@ Route::get('/', function () {
 });
 
 Route::get('/somar', function(Request $request){
+    //nao esta chegando nada pela request
+    if(count($request->all()) < 1 ){
+        return response()->json([
+            'message' => 'Não há valores para somar.'
+        ], 406 );
+    }
+
     $soma = array_sum($request -> all());
     return response()->json([
-        'message' => 'OK', //opcional
+        'message' => 'somando com sucesso', //opcional
         'sum' => $soma,
     ]);
 });
