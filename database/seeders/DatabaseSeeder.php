a57fd740347d313a527ab455bf0d0a814db6931a
brend4ds <104913796+brend4ds@users.noreply.github.com>
2024-10-22 08:49:34 -0300

    aula 22/10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\People;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(10)->create();
+        People::factory(10)->create();
+            // 'name' => 'Test User',
+            // 'email' => 'test@example.com',
+      
     }
 }
