8720af90c50dddec725d6b5e8854387a36c42db7
brend4ds <104913796+brend4ds@users.noreply.github.com>
2024-10-25 11:33:14 -0300

    aula 25/10

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PeopleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,17 +10,24 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/somar', function(Request $request){
+Route::get('/somar', function (Request $request) {
     //nao esta chegando nada pela request
-    if(count($request->all()) < 1 ){
+    if (count($request->all()) < 1) {
         return response()->json([
             'message' => 'Não há valores para somar.'
-        ], 406 );
+        ], 406);
     }
 
-    $soma = array_sum($request -> all());
+    $soma = array_sum($request->all());
     return response()->json([
         'message' => 'somando com sucesso', //opcional
         'sum' => $soma,
     ]);
+    
 });
+
+Route::prefix('/people')->group(function () {
+        Route::get('/list', [PeopleController::class, 'list']);
+
+        Route::post('/store', [PeopleController::class, 'store']);
+    });
