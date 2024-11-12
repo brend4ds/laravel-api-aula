c5c38594f8ca3ee888a3d523f08b21c4898a300b
brend4ds <104913796+brend4ds@users.noreply.github.com>
2024-11-12 08:44:09 -0300

    aula 12/11

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -44,4 +45,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims() {
+        return [];
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\JWTAuthController; 
 use App\Http\Controllers\PeopleController;
+use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 Route::get('/', function () {
     return response()->json([
@@ -26,7 +29,7 @@ Route::get('/somar', function (Request $request) {
     
 });
 
-Route::prefix('/people')->group(function () {
+Route::prefix('/people')->middleware([JwtMiddleware::class])->group(function () {
         Route::get('/list', [PeopleController::class, 'list']);
 
         Route::post('/store', [PeopleController::class, 'store']);//StorePeopleRequest
@@ -34,3 +37,12 @@ Route::prefix('/people')->group(function () {
         Route::post('/storeInterest', [PeopleController::class, 'storeInterest']);
     });
 
+
+Route::prefix('/user')->group(function() {
+    Route::post('/register', [JWTAuthController::class, 'register']);
+
+    Route::post('/login', [JWTAuthController::class, 'login']);
+
+    Route::middleware([JwtMiddleware::class])->get('/logout', [JWTAuthController::class, 'logout']);
+
+});
